1e482d422c2aa951abe7190cfefd81cd02e133a6
Abhi patel <abhimdx1878@gmail.com>
2024-01-15 10:42:14 +0000

    initial commit

--- librarian.h
+++ librarian.h
@@ -6,29 +6,29 @@
 
 class Librarian {
 private:
-    int staffId;
+    int id;
     std::string name;
     std::string address;
     std::string email;
     int salary;
 
 public:
-    // Constructors
-    Librarian(int id, const std::string& name, const std::string& address, const std::string& email, int salary);
+    
+  Librarian(int id, const std::string& name, const std::string& address, const std::string& email, int salary);
 
-    // Setters
-    void setStaffId(int id);
+  void setid (int id);
+   
     void setName(const std::string& name);
     void setAddress(const std::string& address);
     void setEmail(const std::string& email);
     void setSalary(int salary);
 
-    // Getters
-    int getStaffId() const;
+    
+    int getid() const;
     std::string getName() const;
     std::string getAddress() const;
     std::string getEmail() const;
     int getSalary() const;
 };
 
-#endif // LIBRARIAN_H
+#endif 
